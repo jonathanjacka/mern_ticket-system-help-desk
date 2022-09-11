3472c3f800faa560dec145154896a342d69f9cfd
Jonathan Jacka <jonathanjacka@gmail.com>
2022-09-11 06:33:54 -0500

    Added initial views and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
+import Register from './views/Register';
+import Login from './views/Login';
+
+import Header from './components/Header';
 
 function App() {
   return (
-    <div>
-      <h1>Hello there</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
