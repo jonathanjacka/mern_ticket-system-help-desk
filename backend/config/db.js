15e37b18eda8d90b25e937210541f8e79a770d0e
Jonathan Jacka <jonathanjacka@gmail.com>
2022-11-23 21:23:19 -0600

    removed colors dependency

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,10 @@
 const mongoose = require('mongoose');
 const debug = require('debug')('app:database');
-const colors = require('colors');
 
 const connectDB = async () => {
   try {
     const connect = await mongoose.connect(process.env.MONGO_URI);
-    if (process.env.NODE_ENV === 'development') {
-      debug(
-        colors.inverse.blue(`Connected to database: ${connect.connection.host}`)
-      );
-    }
+    debug(`Connected to database: ${connect.connection.host}`);
   } catch (error) {
     debug(colors.inverse.red.bold(`Error: ${error.message}`));
     process.exit(1);
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const debug = require('debug')('app:server');
-const colors = require('colors');
 const dotenv = require('dotenv').config();
 const morgan = require('morgan');
 const connectDB = require('./config/db');
@@ -42,11 +41,7 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 
 const server = app.listen(PORT, () => {
-  if (process.env.NODE_ENV === 'development') {
-    debug(
-      colors.inverse.green(
-        `Server is running in ${process.env.NODE_ENV} on port ${PORT} - Hello there...`
-      )
-    );
-  }
+  debug(
+    `Server is running in ${process.env.NODE_ENV} on port ${PORT} - Hello there...`
+  );
 });
